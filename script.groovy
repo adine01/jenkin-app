f0a0963d3187a91b7aedef7530b5db15c168b864
K.P.D. Anjitha <dineth01a@gmail.com>
2025-01-18 19:10:51 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,20 +1,18 @@
-// Groovy script to compile and run a C++ program
-def cppFile = 'program.cpp'
-def executable = 'program'
-
-// Compile the C++ file
-println "Compiling ${cppFile}..."
-def compileResult = "g++ ${cppFile} -o ${executable}".execute()
-compileResult.waitFor()
-
-if (compileResult.exitValue() == 0) {
-    println "Compilation successful. Running the program..."
-    // Run the compiled executable
-    def runResult = "./${executable}".execute()
-    runResult.in.eachLine { line -> println line }
-    runResult.waitFor()
-    println "Program finished with exit code: ${runResult.exitValue()}"
-} else {
-    println "Compilation failed. Error:"
-    compileResult.err.eachLine { line -> println line }
+pipeline {
+    agent any
+    stages {
+        stage('Checkout Repository') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Compile and Run C++') {
+            steps {
+                bat '''
+                g++ -o program program.cpp
+                program
+                '''
+            }
+        }
+    }
 }
